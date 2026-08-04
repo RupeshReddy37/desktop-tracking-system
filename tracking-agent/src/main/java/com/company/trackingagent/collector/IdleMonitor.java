46f2213d5d616fdabb6b045663d7d18c80d6eb95
RupeshReddy37 <rupeshreddylomada@gmail.com>
2026-08-04 22:20:25 +0530

    fix: resolve compilation and IDE errors

--- tracking-agent/src/main/java/com/company/trackingagent/collector/IdleMonitor.java
+++
@@ -1,5 +0,0 @@
-package com.company.trackingagent.service;
-
-public interface IdleMonitor {
-    long getIdleSeconds();
-}
--- tracking-agent/src/main/java/com/company/trackingagent/collector/WindowMonitor.java
+++
@@ -1,7 +0,0 @@
-package com.company.trackingagent.service;
-
-import com.company.trackingagent.model.ActiveWindowInfo;
-
-public interface WindowMonitor {
-    ActiveWindowInfo getActiveWindow();
-}
